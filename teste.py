377fa661375d3efe104bb578c5bfb0d4657bc4ef
im-a-Cloud <prudencioalvaro10@gmail.com>
2019-02-21 09:59:48 -0300

    Atualização no código emPython

---
+++ teste.py
@@ -0,0 +1,6 @@
+#coding: utf-8
+
+print("Hello THE WORLD!!!\n");
+print("Esse código está no MEU git, mas se pedir com jeitinho eu deixo vc dar um pull request nele\n");
+print("Clique nesse link para baixar um trojan na sua máquina\n");
+print("https://github.com/im-a-Cloud/Tutorial\n");
